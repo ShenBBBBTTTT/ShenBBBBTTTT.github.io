59e6468dc64b5209457c6275ed46810cd08d90ad
ShenBBBBTTTT <193783114+ShenBBBBTTTT@users.noreply.github.com>
2025-01-14 18:54:19 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
-const button = document.querySelector("input");
-const paragraph = document.querySelector("p");
-
-button.addEventListener("click", updateButton);
-
-function updateButton() {
-  if (button.value === "Run") {
-    button.value = "Stop";
-    paragraph.textContent = "Hav run";
-  } else {
-    button.value = "Run";
-    paragraph.textContent = "Hav stop";
+window.onload()=function{
+  const button = document.querySelector("input");
+  const paragraph = document.querySelector("p");
+  
+  button.addEventListener("click", updateButton);
+  
+  function updateButton() {
+    if (button.value === "Run") {
+      button.value = "Stop";
+      paragraph.textContent = "Hav run";
+    } else {
+      button.value = "Run";
+      paragraph.textContent = "Hav stop";
+    }
   }
 }
